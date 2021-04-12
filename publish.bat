125675df6d4044e087d0e92c3e365d6f25e12787
Krishna Chaitanya <kchaitanya556@gmail.com>
2021-04-12 19:36:45 +0530

    Add publish.bat

--- publish.bat
+++ publish.bat
@@ -1,3 +1,7 @@
 git add .
 git commit -m %1
-git push -u origin
+git push -u origin pelicanbranch
+pelican content -o output -s pelicanconf.py
+ghp-import output -r origin -b master
+git push origin master
+git checkout pelicanbranch
