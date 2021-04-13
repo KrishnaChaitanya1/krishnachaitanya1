fd8a6807c514d2cb099ec03604120511c1de4539
Krishna Chaitanya <kchaitanya556@gmail.com>
2021-04-13 19:11:53 +0530

    change makefile

--- publish.bat
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -m %1
-git push -u origin pelicanbranch
-pelican content -o output -s pelicanconf.py
-ghp-import output -r origin -b master
-git push origin master
-git checkout pelicanbranch
